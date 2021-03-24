9ee36e668a3455f8b1398cd8b0f0b15bfcf392d3
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2021-03-24 19:04:45 +0100

    fix: review components in declarations array in decorator of app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,84 +1,79 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, APP_INITIALIZER } from "@angular/core";
-import { KeycloakService, KeycloakAngularModule } from "keycloak-angular";
-import { initializer } from "./utils/app-init";
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule, APP_INITIALIZER } from "@angular/core"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"
+import { HttpClientModule } from "@angular/common/http"
 
-import { AppRoutingModule, routingComponents } from "./app-routing.module";
-// Material designe
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { KeycloakService, KeycloakAngularModule } from "keycloak-angular"
+import { DataTablesModule } from "angular-datatables"
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular"
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import * as c3 from "c3"
+import * as d3 from "d3"
 
-import { StatisticsService } from "./shared/statistics.service";
-import * as c3 from "c3";
-import * as d3 from "d3";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module"
+import { MaterialModule } from "./material.module"
 
-import { TopMenuComponent } from "./top-menu/top-menu.component";
-import { FooterComponent } from "./footer/footer.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { initializer } from "./utils/app-init"
+import { StatisticsService } from "./shared/statistics.service"
+import { environment } from "../environments/environment"
+import { AppComponent } from "./app.component"
+import { TopMenuComponent } from "./top-menu/top-menu.component"
+import { FooterComponent } from "./footer/footer.component"
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component"
+import { DocsComponent } from "./docs/docs.component"
+import { DashboardComponent } from "./dashboard/dashboard.component"
+import { AboutComponent } from "./about/about.component"
+import { PrivateComponent } from "./private/private.component"
+import { StatisticsComponent } from "./statistics/statistics.component"
 
-import { DataTablesModule } from "angular-datatables";
-
-import { HttpClientModule } from "@angular/common/http";
-import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
-import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { environment } from "../environments/environment";
-import { DocsComponent } from "./docs/docs.component";
-
-// env variable to a local variable
-const envurl = environment.SCIENTIFIC_SERVICE_URL;
-
-// function to create apollo client
 export function createApollo(httpLink: HttpLink) {
-    return {
-        link: httpLink.create({ uri: envurl }),
-        cache: new InMemoryCache()
-    };
+  return {
+    link: httpLink.create({ uri: environment.SCIENTIFIC_SERVICE_URL }),
+    cache: new InMemoryCache()
+  }
 }
 
-/**
- * Main app module
- */
 @NgModule({
-    declarations: [
-        AppComponent,
-        TopMenuComponent,
-
-        // all the pages that are in top menu
-        routingComponents,
-        FooterComponent,
-        PageNotFoundComponent,
-        DocsComponent
-    ],
-    imports: [
-        BrowserModule,
-        DataTablesModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MaterialModule,
-        KeycloakAngularModule,
-        HttpClientModule,
-        ApolloModule,
-        HttpLinkModule
-    ],
-    providers: [
-        StatisticsService,
-        {
-            provide: APP_INITIALIZER,
-            useFactory: initializer,
-            multi: true,
-            deps: [KeycloakService]
-        },
-        {
-            provide: APOLLO_OPTIONS,
-            useFactory: createApollo,
-            deps: [HttpLink]
-        }
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopMenuComponent,
+    DashboardComponent,
+    StatisticsComponent,
+    AboutComponent,
+    PrivateComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    DocsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    DataTablesModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    KeycloakAngularModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [
+    StatisticsService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink]
+    }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
